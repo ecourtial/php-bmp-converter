b33a0dc0e268416b975705f69f84de8d1365f5ee
Eric COURTIAL <eric.courtial@wizacha.com>
2021-02-10 08:28:50 +0100

    Compat with PHP8

--- src/Core/PhpCore.php
+++ src/Core/PhpCore.php
@@ -4,14 +4,25 @@ declare(strict_types=1);
 
 namespace Ecourtial\PhpBmpParser\Core;
 
+/**
+ * This class has the purpose to mock native PHP function.
+ * It mus be compatible with both PHP 7.4 and 8.
+ */
 class PhpCore
 {
     public function getFileSize(string $filePath): int
     {
-        $fileSize = @\filesize($filePath);
+        // This code must be compatible with both PHP7.4 and 8.
+        try {
+            $msg = '';
+            $fileSize = @\filesize($filePath);
+        } catch (\Throwable $exception) {
+            $msg = $exception->getMessage();
+            $fileSize = false;
+        }
 
         if (false === is_int($fileSize)) {
-            throw new \Exception('Impossible to assert the size of the given file!');
+            throw new \Exception("Impossible to assert the size of the given file! $msg");
         }
 
         return $fileSize;
@@ -20,10 +31,17 @@ class PhpCore
     /** @phpstan-ignore-next-line */
     public function fopen($filePath)
     {
-        $file = @\fopen($filePath, "rb");
+        // This code must be compatible with both PHP7.4 and 8.
+        try {
+            $msg = '';
+            $file = @\fopen($filePath, "rb");
+        } catch (\Throwable $exception) {
+            $msg = $exception->getMessage();
+            $file = false;
+        }
 
         if (false === is_resource($file)) {
-            throw new \Exception('Impossible to open the given file!');
+            throw new \Exception("Impossible to open the given file! $msg");
         }
 
         return $file;
@@ -32,10 +50,17 @@ class PhpCore
     /** @phpstan-ignore-next-line */
     public function fread($resource, $fileLength): string
     {
-        $raw = @\fread($resource, $fileLength);
+        // This code must be compatible with both PHP7.4 and 8.
+        try {
+            $msg = '';
+            $raw = @\fread($resource, $fileLength);
+        } catch (\Throwable $exception) {
+            $msg = $exception->getMessage();
+            $raw = false;
+        }
 
         if (false === is_string($raw)) {
-            throw new \Exception('Impossible to read the given file!');
+            throw new \Exception("Impossible to read the given file! $msg");
         }
 
         return $raw;
@@ -44,10 +69,17 @@ class PhpCore
     /** @return string[] */
     public function unpack(string $format, string $string): array
     {
-        $data = @\unpack($format, $string);
+        // This code must be compatible with both PHP7.4 and 8.
+        try {
+            $msg = '';
+            $data = @\unpack($format, $string);
+        } catch (\Throwable $exception) {
+            $msg = $exception->getMessage();
+            $data = false;
+        }
 
         if (false === is_array($data)) {
-            throw new \Exception('Impossible to unpack the header of the file!');
+            throw new \Exception("Impossible to unpack the header of the file! $msg");
         }
 
         return $data;
@@ -56,13 +88,21 @@ class PhpCore
     /** @return string[] */
     public function strSplit(string $string, int $length, string $what, string $extra = ''): array
     {
-        $data = @\str_split($string, $length);
+        // This code must be compatible with both PHP7.4 and 8.
+        try {
+            $msg = '';
+            $data = @\str_split($string, $length);
+        } catch (\Throwable $exception) {
+            $msg = $exception->getMessage();
+            $data = false;
+        }
 
         if (false === is_array($data) || 0 === count($data)) {
-            $msg = "Impossible to splint the given string of type: '$what'";
-            $msg = $extra === '' ? $msg : $msg . ' at ' . $extra;
+            $error = "Impossible to splint the given string of type: '$what'";
+            $error = $extra === '' ? $error : $error . ' at ' . $extra;
+            $error .= " $msg";
 
-            throw new \Exception($msg);
+            throw new \Exception($error);
         }
 
         return $data;
@@ -71,10 +111,18 @@ class PhpCore
     /** @phpstan-ignore-next-line */
     public function imageCreateTrueColor(int $width, int $height)
     {
-        $imageFile = @\imagecreatetruecolor($width, $height);
+        // This code must be compatible with both PHP7.4 and 8.
+        try {
+            $msg = '';
+            $imageFile = @\imagecreatetruecolor($width, $height);
+        } catch (\Throwable $exception) {
+            $msg = $exception->getMessage();
+            $imageFile = false;
+        }
 
-        if (false === $imageFile) {
-            throw new \Exception("Impossible to create the image!");
+        /** @phpstan-ignore-next-line */
+        if (false === is_resource($imageFile) && false === is_object($imageFile)) {
+            throw new \Exception("Impossible to create the image! $msg");
         }
 
         return $imageFile;
@@ -83,14 +131,22 @@ class PhpCore
     /** @phpstan-ignore-next-line */
     public function imageColorAllocate($image, int $r, int $g, int $b): int
     {
-        if (false === is_resource($image)) {
-            throw new \Exception('Image must be a resource!');
+        // This code must be compatible with both PHP7.4 and 8.
+        if (false === is_resource($image) && false === is_object($image)) {
+            throw new \Exception('Image must be a resource!' . gettype($image) . ' given.');
         }
 
-        $color = @\imagecolorallocate($image, $r, $g, $b);
+        try {
+            $msg = '';
+            /** @phpstan-ignore-next-line */
+            $color = @\imagecolorallocate($image, $r, $g, $b);
+        } catch (\Throwable $exception) {
+            $msg = $exception->getMessage();
+            $color = false;
+        }
 
         if (false === $color) {
-            throw new \Exception("Impossible to create the color with RGB $r, $g, $b!");
+            throw new \Exception("Impossible to create the color with RGB $r, $g, $b! $msg");
         }
 
         return $color;
